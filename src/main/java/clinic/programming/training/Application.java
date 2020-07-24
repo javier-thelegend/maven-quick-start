046e9fce7b59b429b92ccb270dbb74325778feaa
javier-thelegend <javier.thelegend@gmail.com>
2020-07-24 09:32:24 -0600

    Changing compile pluging and Application java class

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,18 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+	public void lista(){
+		List<String> l = new ArrayList<>();
+		l.add("Test");
+		
+		for(String s : l){
+			System.out.println("Valor: " + s);
+		}
+	}
+	
 	public Application(){
 		System.out.println("Inside Application");
 	}
@@ -8,5 +20,6 @@ public class Application {
 	public static void main(String args[]){
 		System.out.println("Starting Application");
 		Application a = new Application();
+		a.lista();
 	}
 }
